188a1e1af6b02c72a6838a3a28c29f31ea4f4909
aaron-webdev <aaronclure.webdev@gmail.com>
2025-07-04 03:46:41 -0400

    started creating homepage

--- WeatherAPIreact/src/App.jsx
+++ WeatherAPIreact/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>
-        Weather App API call
-      </p>
+    <section className='d-flex flex-column text-center'>
+      <div className='m-auto my-5'>
+        <h1>Weather Warden</h1>
+        <h3>On demand weather reports by city</h3>        
+      </div>
+
+    </section>
     </>
   )
 }
