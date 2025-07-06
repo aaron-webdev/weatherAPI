71f80bf4c5b1100aef06f3e0e9c58a21f4eb29a2
aaron-webdev <aaronclure.webdev@gmail.com>
2025-07-05 21:22:24 -0400

    updated app to display as two columns

--- WeatherAPIreact/src/App.jsx
+++ WeatherAPIreact/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css"
+import "./App.css";
+import {Row, Col} from 'react-bootstrap';
 import WeatherDisplay from './components/WeatherDisplay';
 import InputComponent from './components/InputComponent';
 
@@ -9,15 +10,19 @@ export default function App() {
 
   return (
     <>
-    
     <section className='d-flex flex-column text-center'>
+      <h1>Weather Warden</h1>
+      <h3>On demand weather reports by city</h3>
       <div className='m-auto my-5'>
-        <h1>Weather Warden</h1>
-        <h3>On demand weather reports by city</h3>        
+        <Row>
+          <Col>
+            <WeatherDisplay/>
+          </Col>
+          <Col>
+            <InputComponent/>
+          </Col>
+        </Row>
       </div>
-    <br/>
-      <WeatherDisplay/>
-      <InputComponent/>
     </section>
     </>
   )
--- WeatherAPIreact/src/components/InputComponent.jsx
+++ WeatherAPIreact/src/components/InputComponent.jsx
@@ -19,72 +19,72 @@ export default function InputComponent()
             <section className='d-flex'>
                 <div className='m-auto'>
                     <h4>Input Component</h4>
-                            <Form>
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="New York City"
-                                value="newYorkCity"
-                                checked={city==="newYorkCity"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Boston"
-                                value="boston"
-                                checked={city==="boston"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Detroit"
-                                value="detroit"
-                                checked={city==="detroit"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Los Angeles"
-                                value="losAngeles"
-                                checked={city==="losAngeles"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Houston"
-                                value="houston"
-                                checked={city==="houston"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Nashville"
-                                value="nashville"
-                                checked={city==="nashville"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Knoxville"
-                                value="knoxville"
-                                checked={city==="knoxville"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                            <Form.Check
-                                name="selectLocation"
-                                type="radio"
-                                label="Portland"
-                                value="portland"
-                                checked={city==="portland"}
-                                onChange={(e) => setCity(e.target.value)}
-                            />
-                        </Form>      
+                    <Form>
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="New York City"
+                        value="newYorkCity"
+                        checked={city==="newYorkCity"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Boston"
+                        value="boston"
+                        checked={city==="boston"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Detroit"
+                        value="detroit"
+                        checked={city==="detroit"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Los Angeles"
+                        value="losAngeles"
+                        checked={city==="losAngeles"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Houston"
+                        value="houston"
+                        checked={city==="houston"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Nashville"
+                        value="nashville"
+                        checked={city==="nashville"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Knoxville"
+                        value="knoxville"
+                        checked={city==="knoxville"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <Form.Check
+                        name="selectLocation"
+                        type="radio"
+                        label="Portland"
+                        value="portland"
+                        checked={city==="portland"}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                </Form>      
                 </div>
                 
             </section>
