54ceffbed3274d21a09d8bb4df88312fe9001977
aaron-webdev <aaronclure.webdev@gmail.com>
2025-07-05 20:26:45 -0400

    installed and imported libraries and files

--- WeatherAPIreact/src/components/InputComponent.jsx
+++ WeatherAPIreact/src/components/InputComponent.jsx
@@ -1,10 +1,12 @@
+import {useState, useEffect} from 'react'
 import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 export default function InputComponent()
 {
+    const [city, setCity] = useState('newYorkCity');
 
 
 
@@ -19,6 +21,8 @@ export default function InputComponent()
                             type="radio"
                             label="New York City"
                             value="newYorkCity"
+                        checked={city==="newYorkCity"}
+                        onChange={(e) => setCity(e.target.value)}
                         />
                         <Form.Check
                             name="selectLocation"
