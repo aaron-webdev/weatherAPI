430ff1af6f96bab9e197df903b6a3bb036c64efe
aaron-quickstart <aaron12clure@gmail.com>
2025-07-09 15:43:40 -0400

    update

--- WeatherAPIreact/src/components/WeatherApp.jsx
+++ WeatherAPIreact/src/components/WeatherApp.jsx
@@ -9,14 +9,14 @@ export default function InputComponent()
     const [searchedLongitude, setSearchedLongitude] = useState(-74.0061);
     const APIurl = `https://api.open-meteo.com/v1/forecast?latitude=${searchedLatitude}&longitude=${searchedLongitude}&current=weather_code,temperature_2m,apparent_temperature,precipitation,relative_humidity_2m,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=auto&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch`;
     const cityCoords = {
-        "newYorkCity": {cityLatitude: 40.7128, cityLongitude: -74.0061},
-        "boston": {cityLatitude: 42.3604, cityLongitude: -71.0580},
-        "detroit": {cityLatitude: 42.3297, cityLongitude: -83.0425},
-        "losAngeles": {cityLatitude: 34.0537, cityLongitude: -118.2427},
-        "houston": {cityLatitude: 29.7602, cityLongitude: -95.3694},
-        "nashville": {cityLatitude: 36.1673, cityLongitude: -86.7784},
-        "knoxville": {cityLatitude: 35.964668, cityLongitude: -83.9264},
-        "portland": {cityLatitude: 45.5152, cityLongitude: -122.6784},
+        "newYorkCity": {cityLatitude: 40.7128, cityLongitude: -74.0061, cityName: 'New York City'},
+        "boston": {cityLatitude: 42.3604, cityLongitude: -71.0580, cityName: 'Boston'},
+        "detroit": {cityLatitude: 42.3297, cityLongitude: -83.0425,  cityName: 'Detroit'},
+        "losAngeles": {cityLatitude: 34.0537, cityLongitude: -118.2427,  cityName: 'Los Angeles'},
+        "houston": {cityLatitude: 29.7602, cityLongitude: -95.3694,  cityName: 'Houston'},
+        "nashville": {cityLatitude: 36.1673, cityLongitude: -86.7784,  cityName: 'Nashville'},
+        "knoxville": {cityLatitude: 35.964668, cityLongitude: -83.9264,  cityName: 'Knoxville'},
+        "portland": {cityLatitude: 45.5152, cityLongitude: -122.6784,  cityName: 'Portland'},
     };
     let currentTime = '';
     let currentWeatherCOde = 0;
@@ -27,7 +27,6 @@ export default function InputComponent()
     let currentWindDirection = '';
     let currentWindSpeed = 0;
     let currentWindGust = 0;
-    //precipitaction, preassure, weather code, wind direction, wind gusts, 
 
 
 
@@ -68,9 +67,44 @@ useEffect(() => {
     });
 }, [searchedLatitude, searchedLongitude]);
 
+function showWeatherData(weatherInfo)
+{
+    currentTime = weatherInfo.current.time;
+    currentWeatherCOde = 0;
+    currentTemperature = 0;
+    currentRelHumidity = 0;
+    currentSurfacePressure = 0;
+    currentPrecipitation = 0;
+    currentWindDirection = '';
+    currentWindSpeed = 0;
+    currentWindGust = 0;
+}
+
 
 return(
     <>
+        <Button onClick={showWeatherData(weatherInfo)}>Get Weather Report</Button>
+        <section id='displayComponent'>
+            <div>
+                <h4>Weather Report as of: {currentTime}</h4>
+                <h6>{cityCoords[city].cityName}</h6>
+                <p>Weather Code:</p>
+                <p>Current Temperature:</p>
+                <p></p>
+                    
+   {/*
+    let currentTime = '';
+    let currentWeatherCOde = 0;
+    let currentTemperature = 0;
+    let currentRelHumidity = 0;
+    let currentSurfacePressure = 0;
+    let currentPrecipitation = 0;
+    let currentWindDirection = '';
+    let currentWindSpeed = 0;
+    let currentWindGust = 0;
+   */} 
+            </div>
+        </section>
         <section className=' border border-3 border-black rounded-3 p-3'>
             <div id='citySelectRadiosDiv'className='m-auto'>
                 <h5>Select your city</h5>
@@ -152,52 +186,6 @@ return(
 
 /*
 
-<p>{data.data.current.temperature_2m}</p>
 
-<Form.Check
-name="selectLocation"
-type="radio"
-label=""
-value=""
-/>
-Future implementation
-<section id='customCityDiv'>
-    <Form.Check
-        name="selectLocation"
-        type="radio"
-        label="Custom Location:"
-        value="customLocation"
-        checked={city==="customLocation"}
-        onChange={(e) => setCity(e.target.value)}
-    />
-    <div className='d-flex'>
-        <Row>
-            <Col>
-            <label htmlFor='customLatitude'>Latitude:</label>
-            </Col>
-            <Col>
-            <input
-                placeholder='Latitude:'
-                id='customLatitude'
-                value={customLatitude}
-                onChange={(e) => setCustomLatitude(e.target.value)}
-            />
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-            <label htmlFor='customLongitude'>Longitude:</label>
-            </Col>
-            <Col>
-            <input
-                placeholder='Longitude:'
-                id='customLongitude'
-                value={customLongitude}
-                onChange={(e) => setCustomLongitude(e.target.value)}
-            />
-            </Col>
-        </Row>
-    </div>
-    <p>(use negative values for west and south coordinates)</p>
-</section>
+
 */
